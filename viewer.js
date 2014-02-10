bc512894b5efb73a71e87ee8b030d882adb9c667
Daniel Jones <dj311@cam.ac.uk>
2014-02-10 19:06:33 +0000

    changed pdfclipviewer files to just viewer
    since this is now used to render whole textbooks

--- viewer.js
+++ viewer.js
@@ -1,45 +1,50 @@
 /*
-    pdfclipviewer
-    =============
-    Aim of this is to render a snippet of a PDF on a page. Seems to work mostly.... Co-ordinates 
-    (x1,y1) give top left corner, (x2,y2) give bottom right corner. These are used as fractions of 
-    the whole page (I thought this would be useful for scaling later..?).
+    viewer.js (dj311)
+    =================
+    This file contains all procedures related to the rendering of textbook files within a page.
 
-    This is done by modifying the height and width of the canvas element that pdf.js renders to. It 
-    then uses canvas.translate to move to the correct section of the page.
+    If you have a textbook object already, use renderTextbook(), otherwise loadJsonTextbook() will 
+    generate one for you from a JSON source file.
 
     This is contains much code from https://github.com/mozilla/pdf.js/tree/master/examples/helloworld 
     within the function renderPdfClip.
 */
 
+/*Note: the 'container' variable refers to an actual DOM element, not its ID*/
 function loadJsonTextbook(container, source) {
     // Loads JSON textbook, calls inline function on success
     $.getJSON( source, function(textbook) {
-        var title = document.createElement('h1');
-        title.textContent = textbook.title;
-        container.appendChild(title);
-
-        var author = document.createElement('span');
-        author.textContent = textbook.author;
-        author.className += "author";
-        container.appendChild(author);
-
-        $.each(textbook.elements, function( index, element ) {
-            //Create element container
-            var element_container = document.createElement('div');
-            element_container.id = container.id+'_'+index;
-            container.appendChild(element_container);
-            //Render if we know how
-            switch (element.type) {
-                case 'pdf': 
-                    renderPdfElement(element_container, element.source, element.start, element.end);
-                    break;
-                default:
-                    break;
-            }
-        });
+        renderTextbook(container, textbook);
     });
 }
+
+/*Note the 'container' variable refers to an actual DOM element, not its ID*/
+function renderTextbook(container, textbook) {
+    var title = document.createElement('h1');
+    title.textContent = textbook.title;
+    container.appendChild(title);
+
+    var author = document.createElement('span');
+    author.textContent = 'By '+textbook.author;
+    author.className += "author";
+    container.appendChild(author);
+
+    $.each(textbook.elements, function( index, element ) {
+        //Create element container
+        var element_container = document.createElement('div');
+        element_container.id = container.id+'_'+index;
+        container.appendChild(element_container);
+        //Render if we know how
+        switch (element.type) {
+            case 'pdf': 
+                renderPdfElement(element_container, element.source, element.start, element.end);
+                break;
+            default:
+                break;
+        }
+    });
+}
+
 function renderPdfElement(container, pdf_url, start, end) {
     //Unpack start and end variables
     var start_page = Math.floor(start);
@@ -63,46 +68,46 @@ function renderPdfElement(container, pdf_url, start, end) {
         }
     };
 }
+
+/*
+renderPdfClip
+-------------
+Aim of this is to render a snippet of a PDF on a page. Seems to work mostly.... 
+This is done based on a start_y, and en_y of a page, on a page by page basis. 
+These are used as fractions of the whole page (.
+
+This is done by modifying the height and width of the canvas element that pdf.js renders to. It 
+then using canvas.translate to move to the correct section of the page.
+*/
 function renderPdfClip(canvas_id, pdf_url, page_no, y1, y2) {
-    //
     // NOTE:
     // Using a PDF from another server will likely *NOT* work. Because of browser
     // security restrictions, we have to use a file server with special headers
     // (CORS) - most servers don't support cross-origin browser requests.
-    //
+    // ----> why we're making a locally run web application.
 
-    //
     // Disable workers to avoid yet another cross-origin issue (workers need the URL of
     // the script to be loaded, and dynamically loading a cross-origin script does
     // not work)
-    //
     PDFJS.disableWorker = true;
 
-    //
     // Asynchronous download PDF as an ArrayBuffer
-    //
     PDFJS.getDocument(pdf_url).then(function getClipViewer(pdf) {
-      //
       // Fetch the first page
-      //
       pdf.getPage(page_no).then(function getPageClipViewer(page) {
         var scale = 1.5;
         var viewport = page.getViewport(scale);
 
-        //
         // Prepare canvas using PDF page dimensions
-        //
         var canvas = document.getElementById(canvas_id);
         var context = canvas.getContext('2d');
 
-        //The code I (Dan) actually wrote... solid 3 lines....
+        // Displacement and clipping of pages
         canvas.height = viewport.height*(y2-y1);
         canvas.width = viewport.width;
         context.translate(0,-(viewport.height*y1));
 
-        //
         // Render PDF page into canvas context
-        //
         page.render({canvasContext: context, viewport: viewport});
       });
     });
