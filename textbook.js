ae0c52f488c44bdb21d29fe9ff5a0ffb09a60f50
superlopuh <superlopuh@gmail.com>
2014-02-13 17:55:15 +0000

    functions to add/delete elements and modify order

--- textbook.js
+++ textbook.js
@@ -19,18 +19,81 @@ function addPDFRectangleClip(source,startX,startY,endX,endY) {
 	newElement.endX 	= endX;
 	newElement.endY 	= endY;
 	brackets.currentTextbook.elements[] = newElement;
-	// return textbook;
 }
 
 
 
-function addYouTubeVid(textbook,source,offset) {}
+function addYouTubeVid(source,startTime) {
+	var newElement 		= {};
+	newElement.type 	= "youtube";
+	newElement.source 	= source;
+	newElement.startTime 	= startTime;
+	brackets.currentTextbook.elements[] = newElement;
+}
 
-function addText(textbook,text) {}
+function addText(text) {
+	var newElement 		= {};
+	newElement.type 	= "text";
+	newElement.text 	= text;
+	brackets.currentTextbook.elements[] = newElement;
+}
 
-function addWiki(textbook,source) {}
+function addWiki(source) {
+	var newElement 		= {};
+	newElement.type 	= "wiki";
+	newElement.source 	= source;
+	brackets.currentTextbook.elements[] = newElement;
+}
 
-function deleteElement(textbook, clipNumber) {}
+// If within range, delete. Else skip
+function deleteElement(clipNumber) {
+	var numberOfElements = brackets.currentTextbook.elements.length;
+	if (clipNumber >= 0 && clipNumber<numberOfElements) {
+		var newElements = [];
+		for (var el = 0; el < n; el++) {
+			newElements[] = brackets.currentTextbook.elements[el]
+		}
+		for (var el = n+1; el < numberOfElements; el++) {
+			newElements[] = brackets.currentTextbook.elements[el]
+		}
+		brackets.currentTextbook.elements = newElements;
+	}
+}
 
-// Move element at index oldPosition in between element at index NewPosition and the element at index (newPosition+1)
-function moveElement(textbook,oldPosition,newPosition) {}
+// Move element at index oldPosition in between element at index newPosition and the element at index (newPosition+1)
+function moveElement(oldPosition,newPosition) {
+	var numberOfElements = brackets.currentTextbook.elements.length;
+	if (oldPosition >= 0 && oldPosition<numberOfElements
+		newPosition >= 0 && newPosition<numberOfElements) {
+		// If no change then return
+		if (oldPosition == newPosition) {return;}
+		// Copy everything that's not going to be changed
+		var newElements = [];
+		var firstChange = Math.min(oldPosition,newPosition);
+		for (var el = 0; el < firstChange; el++) {
+			newElements[] = brackets.currentTextbook.elements[el];
+		}
+		// If move up the page
+		if (oldPosition > newPosition) {
+			newElements[] = brackets.currentTextbook.elements[oldPosition];
+			for (var el = newPosition; el < oldPosition; el++) {
+				newElements[] = brackets.currentTextbook.elements[el]
+			}
+			for (var el = oldPosition+1; el < numberOfElements; el++) {
+				newElements[] = brackets.currentTextbook.elements[el]
+			}
+			brackets.currentTextbook.elements = newElements;
+		}
+		// If move down the page
+		else {
+			for (var el = oldPosition+1; el <= newPosition; el++) {
+				newElements[] = brackets.currentTextbook.elements[el]
+			}
+			newElements[] = brackets.currentTextbook.elements[oldPosition];
+			for (var el = newPosition+1; el < numberOfElements; el++) {
+				newElements[] = brackets.currentTextbook.elements[el]
+			}
+			brackets.currentTextbook.elements = newElements;
+		}
+	};
+}
